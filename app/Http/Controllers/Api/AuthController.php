44a555d03648439fc867b6722b5028e3021fc660
Swoosky <agushusky@gmail.com>
2021-12-03 21:34:14 +0800

    edit fillable user

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -30,6 +30,7 @@ class AuthController extends Controller
         }
 
         $registrationData['password'] = bcrypt($request->password);
+        $registrationData['img_url'] = 'https://www.seekpng.com/png/detail/966-9665493_my-profile-icon-blank-profile-image-circle.png';
         $user = User::Create($registrationData);
         return response([
             'message' => 'Register Success',
